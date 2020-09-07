9346ea7b94b575d10de7183d8eca340eb05a80ef
Areem-Shrestha <arim.sht@gmail.com>
2020-09-07 08:38:22 +0545

    slove-68 or moonlist-organims.

---
+++ coinmarketthat/src/components/organisms/MoonList/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import MoonList from './index';
+
+storiesOf('organism/MoonList').add(
+  'White', () => <MoonList theme="white" />
+).add(
+  'Dark', () => <MoonList theme="dark" />
+);
